5ee5d18186a241278594c774cb9876dbd2982cf4
Loudwige Odice <55907221+Gaby077@users.noreply.github.com>
2022-04-29 03:04:32 +0800

    create services, service tests, and seeds

---
+++ spec/integration/service_add_accessor_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+
+describe 'Test AddAccessorToLink service' do
+  before do
+    wipe_database
+
+    DATA[:accounts].each do |account_data|
+      EtaShare::Account.create(account_data)
+    end
+
+    link_data = DATA[:links].first
+
+    @sender = EtaShare::Account.all[0]
+    @accessoor = EtaShare::Account.all[1]
+    @link = EtaShare::CreateLinkForSender.call(
+      sender_id: @sender.id, link_data:
+    )
+  end
+
+  it 'HAPPY: should be able to add an accessor to a link' do
+    EtaShare::AddAccessorToLink.call(
+      email: @accessor.email,
+      link_id: @link.id
+    )
+
+    _(@accessor.links.count).must_equal 1
+    _(@accessor.links.first).must_equal @link
+  end
+
+  it 'BAD: should not add sender as an accessor' do
+    _(proc {
+      EtaShare::AddAccessorToLink.call(
+        email: @sender.email,
+        link_id: @link.id
+      )
+    }).must_raise EtaShare::AddAccessorToLink::SenderNotAccessorError
+  end
+end
