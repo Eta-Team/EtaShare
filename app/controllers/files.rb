c93ec4e4369a328dd528dcf10187502d6596e114
Emmy Avilez <avileze@yahoo.com>
2022-06-08 12:53:14 +0800

    Add files controller

---
+++ app/controllers/files.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative './app'
+
+module EtaShare
+  # File Web controller for Credence API
+  class Api < Roda
+    route('files') do |routing|
+      routing.halt 403, { message: 'Not authorized' }.to_json unless @auth_account
+
+      @file_route = "#{@api_root}/files"
+
+      # GET api/v1/files/[file_id]
+      routing.on String do |file_id|
+        @req_file = File.first(id: file_id)
+
+        routing.get do
+          file = GetFileQuery.call(
+            requestor: @auth_account, file: @req_file
+          )
+
+          { data: file }.to_json
+        rescue GetFileQuery::ForbiddenError => e
+          routing.halt 403, { message: e.message }.to_json
+        rescue GetFileQuery::NotFoundError => e
+          routing.halt 404, { message: e.message }.to_json
+        rescue StandardError => e
+          puts "GET DOCUMENT ERROR: #{e.inspect}"
+          routing.halt 500, { message: 'API server error' }.to_json
+        end
+      end
+    end
+  end
+end
