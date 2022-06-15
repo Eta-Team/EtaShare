b628777ae9ae50a7859c7c40e124be70a0a98713
Loudwige Odice <55907221+Gaby077@users.noreply.github.com>
2022-06-15 13:26:57 +0800

    Require signed requests for non-authenticated route

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,11 +7,16 @@ module EtaShare
   # Web controller for EtaShare API
   class Api < Roda
     route('auth') do |routing|
+      begin
+        @request_data = SignedRequest.new(Api.config).parse(request.body.read)
+      rescue SignedRequest::VerificationError
+        routing.halt '403', { message: 'Must sign request' }.to_json
+      end
+      
       routing.on 'register' do
         # POST api/v1/auth/register
-        routing.post do
-          reg_data = JsonRequestBody.parse_symbolize(request.body.read)
-          VerifyRegistration.new(reg_data).call
+       routing.post do
+          VerifyRegistration.new(@request_data).call
 
           response.status = 202
           { message: 'Verification email sent' }.to_json
@@ -24,26 +29,24 @@ module EtaShare
           routing.halt 500
         end
       end
+      end
 
-      routing.is 'authenticate' do
+          routing.is 'authenticate' do
         # POST /api/v1/auth/authenticate
         routing.post do
-          credentials = JsonRequestBody.parse_symbolize(request.body.read)
-          auth_account = AuthenticateAccount.call(credentials)
+          auth_account = AuthenticateAccount.call(@request_data)
           { data: auth_account }.to_json
         rescue AuthenticateAccount::UnauthorizedError
-          routing.halt '403', { message: 'Invalid credentials' }.to_json
+          routing.halt '401', { message: 'Invalid credentials' }.to_json
         end
       end
 
       routing.post 'sso' do
-        auth_request = JsonRequestBody.parse_symbolize(request.body.read)
-
-        auth_account = AuthorizeSso.new(Api.config).call(auth_request[:id_token])
+        auth_account = AuthorizeSso.new.call(@request_data[:access_token])
         { data: auth_account }.to_json
-      rescue StandardError => e
-        puts "FAILED to validate Google account: #{e.inspect}"
-        puts e.backtrace
+      rescue StandardError => error
+        puts "FAILED to validate Google account: #{error.inspect}"
+        puts error.backtrace
         routing.halt 400
       end
     end
